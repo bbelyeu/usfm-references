4ec92972fd8f7646b4020785f5e3c8d81b6245d6
bbelyeu <bradley.belyeu@life.church>
2019-11-13 13:49:11 -0600

    Rev version

--- usfm_references/__init__.py
+++ usfm_references/__init__.py
@@ -3,7 +3,7 @@ USFM References Tools
 """
 import re
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 ANY_REF = re.compile(r'^[1-9A-Z]{3}\.([0-9]{1,3}(_[0-9]+)?(\.[0-9]{1,3})?|INTRO\d+)$')
 CHAPTER = re.compile(r'^[1-6A-Z]{3}\.[0-9]{1,3}(_[0-9]+)?$')
