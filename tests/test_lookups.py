ea05c44e8fd80d0a62909c7a280710882e9b3c9c
musicboy111 <145411075+musicboy111@users.noreply.github.com>
2024-07-28 23:21:47 +0000

    New querys added

--- tests/test_lookups.py
+++ tests/test_lookups.py
@@ -16,3 +16,16 @@ def test_book_name_lookup():
 def test_book_lookup_by_usfm(query, expect):
     """This tests asserts books usfms can be looked up by their USFM"""
     assert usfm_references.book_lookup(query) == expect
+
+
+def test_book_lookup_by_abbr():
+    """This tests asserts books can be looked up by their abbrviation"""
+    assert usfm_references.book_lookup("Jsh") == "JOS"
+    assert usfm_references.book_lookup("Rth") == "RUT"
+    assert usfm_references.book_lookup("Jdgs") == "JUD"
+    assert usfm_references.book_lookup("Jb") == "JOB"
+    assert usfm_references.book_lookup("Ps") == "PSA"
+    assert usfm_references.book_lookup("Prv") == "PRO"
+    assert usfm_references.book_lookup("Ezk") == "EZE"
+    assert usfm_references.book_lookup("Am") == "AMO"
+    assert usfm_references.book_lookup("Esth") == "EST"
