4cdccd73a3b84b29cda002c41a95d1cc8c47e855
Brad Belyeu <bradleylamar@gmail.com>
2024-07-28 22:22:51 +0000

    Added tests for looking up a book usfm by its usfm

--- tests/test_lookups.py
+++ tests/test_lookups.py
@@ -8,3 +8,11 @@ def test_book_name_lookup():
     """This function tests the book name lookup functionality."""
     book_usfm = usfm_references.book_lookup("Matt")
     assert book_usfm == "MAT"
+
+@pytest.mark.parametrize(
+    "query,expect",
+    [(i, i) for i in usfm_references.BOOKS] + [("ZZZ", None), ("zzz", None)],
+)
+def test_book_lookup_by_usfm(query, expect):
+    """This tests asserts books usfms can be looked up by their USFM"""
+    assert usfm_references.book_lookup(query) == expect
